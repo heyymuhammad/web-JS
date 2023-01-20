9405e321d4781236347405242e66019176f89a65
heyymuhammad <muawan541@gmail.com>
2023-01-20 20:47:21 +0500

    day 9

---
+++ no9-different-concepts.js
@@ -0,0 +1,41 @@
+// for(let i=0;i<21;i++){
+//     if (i%2==0){
+//         document.write(i+"<br>");
+//     }
+// }
+// document.write("<br>")
+// for(let j=0;j<21;j++){
+//     if (j%2!=0){
+//         document.write(j+"<br>");
+//     }
+// }
+
+// Ternary Operator
+
+// x = 10;
+// y = 20;
+
+// Check = x==y ? "Equal" : "Not Equal"
+// document.write(Check)
+
+// Function Parameter
+// user_num1= parseInt(prompt("Enter the first number"));
+// user_num2= parseInt(prompt("Enter the second number"));
+
+function add(num1, num2){
+    alert(num1+num2);
+}
+function sub(num1, num2){
+    alert(num1-num2);
+}
+function divide(num1, num2){
+    alert(num1/num2);
+}
+function product(num1, num2){
+    alert(num1*num2);
+}
+function mode(num1, num2){
+    alert(num1%num2);
+}
+
+
